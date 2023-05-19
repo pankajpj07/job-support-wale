d483299dbb63cfc41a7fc5a1d9753a1a4b5a7d3d
Pankaj Jain <pankaj12.pj88@gmail.com>
2023-05-20 01:05:39 +0530

    Changed a tag to Link tag

--- my-application/src/components/Button.tsx
+++ my-application/src/components/Button.tsx
@@ -1,11 +1,12 @@
 import { ButtonProps } from '@/types/types'
+import Link from 'next/link'
 
 export default function Button({ title, href }: ButtonProps) {
   return (
-    <a href={href}>
+    <Link href={href}>
       <button className="p-3 m-1.5 flex md:w-56 justify-center bg-indigo-700 text-white hover:bg-indigo-600 rounded-full">
         {title}
       </button>
-    </a>
+    </Link>
   )
 }
