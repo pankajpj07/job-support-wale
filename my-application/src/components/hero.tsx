16b730ce4e25e2ed7753777c9da939143d4d9252
Pankaj Jain <47389183+pankajpj07@users.noreply.github.com>
2024-07-29 18:12:39 +0000

    fix(css): changed styling for the buttong in mobile view

--- my-application/src/components/hero.tsx
+++ my-application/src/components/hero.tsx
@@ -3,9 +3,19 @@ import Image from 'next/image'
 import Button from './Button'
 import { heroImage } from '@/constants/constants'
 
+// Define an array of button links and titles
+const buttonData = [
+  { href: "/india-job-support/frontend-support", title: "Frontend Support" },
+  { href: "/india-job-support/backend-support", title: "Backend Support" },
+  { href: "/india-job-support/full-stack-support", title: "FullStack Support" },
+  { href: "/india-job-support/devops-support", title: "DevOps Support" },
+  { href: "/india-job-support/dotnet-support", title: "Dotnet Support" },
+  { href: "/job-support", title: "Other Job Support" }
+];
+
 const HeroBanner = () => {
   return (
-    <div className="relative h-screen -mt-11">
+    <div className="relative h-70vh md:h-screen -mt-11">
       <Image
         src={heroImage}
         alt="Hero Image"
@@ -13,34 +23,20 @@ const HeroBanner = () => {
         quality={100}
         className="object-cover opacity-80"
       />
-      <div className="bg-black bg-opacity-70 absolute inset-0"></div>{' '}
-      {/* Dark overlay */}
+      <div className="bg-black bg-opacity-70 absolute inset-0"></div> {/* Dark overlay */}
       <div className="absolute inset-0 flex flex-col justify-center items-center text-white p-5 md:p-0">
-        <h1 className="text-4xl font-bold mb-10">
+        <h1 className="text-2xl md:text-4xl font-bold mb-5 md:mb-10 text-center">
           GET JOB SUPPORT FROM INDIA BY EXPERIENCED PROFESSIONALS
         </h1>
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-10 justify-center">
-          <Button
-            href="/india-job-support/frontend-support"
-            title="Frontend Job Support"
-          />
-          <Button
-            href="/india-job-support/backend-support"
-            title="Backend Job Support"
-          />
-          <Button
-            href="/india-job-support/full-stack-support"
-            title="Full Stack Job Support"
-          />
-          <Button
-            href="/india-job-support/devops-support"
-            title="DevOps Job Support"
-          />
-          <Button
-            href="/india-job-support/dotnet-support"
-            title="Dotnet Job Support"
-          />
-          <Button href="/job-support" title="Other Job Support" />
+        <div className="flex flex-wrap justify-center w-full max-w-screen-md px-5">
+          {buttonData.map((button, index) => (
+            <div key={index} className="w-1/2 sm:w-1/2 md:w-1/3 p-2">
+              <Button
+                href={button.href}
+                title={button.title}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- my-application/tailwind.config.js
+++ my-application/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      height: {
+        '70vh': '70vh',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
