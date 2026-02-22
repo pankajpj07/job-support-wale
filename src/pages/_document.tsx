93320f2ba6d94f28257da8b7ed4ea91e422c83d9
Pankaj Jain <pankaj12.pj88@gmail.com>
2026-02-22 21:29:43 +0530

    added github publisher account

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-7094724431832534" />
+      </Head>
       <body>
         <Main />
         <NextScript />
