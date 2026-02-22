d79169cb1a1573052d1d9baafd550d6ee2b3c689
Pankaj Jain <pankaj12.pj88@gmail.com>
2026-02-22 21:32:45 +0530

    added adsense script to load ads

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,11 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="google-adsense-account" content="ca-pub-7094724431832534" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7094724431832534"
+          crossOrigin="anonymous"
+        />
       </Head>
       <body>
         <Main />
